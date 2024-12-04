40123e394a36d14ecd9d7feedfb0690e8ce1213b
Andrei <andreimititelu97@gmail.com>
2024-12-05 00:00:08 +0200

    Minor changes

--- backend/src/main/java/org/greatreads/dto/UserDTO.java
+++ backend/src/main/java/org/greatreads/dto/UserDTO.java
@@ -6,20 +6,20 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public class UserDTO {
-    @NotNull(message = "email must not be null")
+    @NotNull(message = "email must not be null.")
     @Email
     private String email;
 
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$",
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–{}:;',?/*~$^+=<>]).{8,20}$",
             message = "password must have digits, punctuation, symbols, lowercase and uppercase characters. " +
                     "Length must be between 8 and 20 characters.")
     private String password;
 
-    @NotNull(message = "First name must not be null")
-    @NotBlank(message = "First name must not be blank")
+    @NotNull(message = "First name must not be null.")
+    @NotBlank(message = "First name must not be blank.")
     private String firstName;
 
-    @NotNull(message = "Last name must not be null")
-    @NotBlank(message = "Last name must not be blank")
+    @NotNull(message = "Last name must not be null.")
+    @NotBlank(message = "Last name must not be blank.")
     private String lastName;
 }
