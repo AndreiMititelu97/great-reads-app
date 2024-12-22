ca5b9264bffe52d082096129aad75183e89a1da4
Andrei <andreimititelu97@gmail.com>
2024-12-22 18:55:56 +0200

    updated init db script

--- db/initDB.sql
+++ db/initDB.sql
@@ -1,14 +1,14 @@
--- CREATE DATABASE greatreads;
+CREATE DATABASE greatreads;
 
-use greatreads;
+USE greatreads;
 
 -- ROLES table
 DROP TABLE IF EXISTS `roles`;
-CREATE TABLE `roles` (
+CREATE TABLE `roles`(
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(50) NOT NULL UNIQUE,
 	PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- GENRES table
 DROP TABLE IF EXISTS `genres`;
@@ -16,11 +16,11 @@ CREATE TABLE `genres` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(75) NOT NULL UNIQUE,
 	PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- USERS Table
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+CREATE TABLE `users`(
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`email` VARCHAR(255) UNIQUE NOT NULL,
 	`password` VARCHAR(255) NOT NULL,
@@ -53,7 +53,7 @@ CREATE TABLE `books`(
 	PRIMARY KEY(`id`),
 	FOREIGN KEY(`user_id`) REFERENCES users(`id`),
 	FOREIGN KEY(`genre_id`) REFERENCES genres(`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- REVIEWS Table
 DROP TABLE IF EXISTS `reviews`;
@@ -69,7 +69,7 @@ CREATE TABLE `reviews` (
 	PRIMARY KEY(`id`),
 	FOREIGN KEY (`book_id`) REFERENCES books(`id`),
     FOREIGN KEY (`user_id`) REFERENCES users(`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- USER_BOOKS table
 DROP TABLE IF EXISTS `user_books`;
@@ -83,7 +83,7 @@ CREATE TABLE `user_books`(
 	PRIMARY KEY(`id`),
 	FOREIGN KEY (`book_id`) REFERENCES books(`id`),
     FOREIGN KEY (`user_id`) REFERENCES users(`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Insert initial roles
 INSERT INTO `roles`(name) VALUES
@@ -114,7 +114,7 @@ INSERT INTO `books`(title, description, genre_id, ISBN, user_id, published_date,
 ('Island', 'In Island, his last novel, Huxley transports us to a Pacific island where, for 120 years, an ideal society has flourished.', 5, '0061561797', 2, '1962-1-1', 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', TRUE, 'https://m.media-amazon.com/images/I/71zv1NFS4qL._SY466_.jpg'),
 ('20,000 Thousand Leagues Under the Sea', 'Jules Verne was remarkably successful in foretelling the wonders science held for the future. This, his most famous novel, earned him the title of "Father of Science Fiction."', 2, '9781433213526', 2, '1869-1-1', 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', TRUE, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348014023i/3185559.jpg');
 
--- Insert initial user_books relationshis
+-- Insert initial user_books relationships
 INSERT INTO `user_books`(user_id, book_id, isRead, isWishlist, read_date) VALUES
 (3, 2, TRUE, FALSE, '2024-1-19'),
 (3, 1, FALSE, TRUE, NULL),
