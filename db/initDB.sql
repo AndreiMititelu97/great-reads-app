11312326df54e2376870efe32c07be1c651e2185
Andrei <andreimititelu97@gmail.com>
2024-12-23 02:29:55 +0200

    Updated table names

--- db/initDB.sql
+++ db/initDB.sql
@@ -27,7 +27,7 @@ CREATE TABLE `users`(
 	`first_name` VARCHAR(255) NOT NULL,
 	`last_name` VARCHAR(255) NOT NULL,
 	`role_id` INTEGER NOT NULL,
-	`isBlocked` BOOLEAN DEFAULT FALSE,
+	`is_blocked` BOOLEAN DEFAULT FALSE,
 	`avatar` VARCHAR(255),
 	`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -46,7 +46,7 @@ CREATE TABLE `books`(
 	`user_id` INTEGER NOT NULL,
 	`published_date` DATE NOT NULL,
 	`url_link` VARCHAR(255),
-	`isApproved` BOOLEAN DEFAULT FALSE,
+	`is_approved` BOOLEAN DEFAULT FALSE,
 	`page_cover` VARCHAR(255),
 	`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -77,9 +77,11 @@ CREATE TABLE `user_books`(
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`user_id` INT NOT NULL,
 	`book_id` INT NOT NULL,
-	`isRead` BOOLEAN DEFAULT FALSE,
-	`isWishlist` BOOLEAN DEFAULT FALSE,
+	`is_read` BOOLEAN DEFAULT FALSE,
+	`is_wishlist` BOOLEAN DEFAULT FALSE,
 	`read_date` DATE,
+	`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY(`id`),
 	FOREIGN KEY (`book_id`) REFERENCES books(`id`),
     FOREIGN KEY (`user_id`) REFERENCES users(`id`)
@@ -102,20 +104,20 @@ INSERT INTO `genres`(name) VALUES
 ('Science');
 
 -- Insert initial users
-INSERT INTO `users`(email, password, first_name, last_name, role_id, isBlocked, avatar) VALUES
+INSERT INTO `users`(email, password, first_name, last_name, role_id, is_blocked, avatar) VALUES
 ('admin@mail.com', 'hashPassword1', 'Kyrie', 'Irving', 1, FALSE, NULL),
 ('author@mail.com', 'hashPassword2', 'Jayson', 'Tatum', 2, FALSE, NULL),
 ('reader@mail.com', 'hashPassword3', 'Al', 'Horford', 3, FALSE, NULL);
 
 
 -- Insert initial books
-INSERT INTO `books`(title, description, genre_id, ISBN, user_id, published_date, url_link, isApproved, page_cover) VALUES
+INSERT INTO `books`(title, description, genre_id, ISBN, user_id, published_date, url_link, is_approved, page_cover) VALUES
 ('A Mind for Numbers', 'In A Mind for Numbers, Dr. Oakley lets us in on the secrets to effectively learning math and science—secrets that even dedicated and successful students wish they’d known earlier.', 7, '0593419057', 2, '2014-07-31', 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', TRUE, 'https://m.media-amazon.com/images/I/81xk6jafTJL._SY466_.jpg'),
 ('Island', 'In Island, his last novel, Huxley transports us to a Pacific island where, for 120 years, an ideal society has flourished.', 5, '0061561797', 2, '1962-1-1', 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', TRUE, 'https://m.media-amazon.com/images/I/71zv1NFS4qL._SY466_.jpg'),
 ('20,000 Thousand Leagues Under the Sea', 'Jules Verne was remarkably successful in foretelling the wonders science held for the future. This, his most famous novel, earned him the title of "Father of Science Fiction."', 2, '9781433213526', 2, '1869-1-1', 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', TRUE, 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348014023i/3185559.jpg');
 
 -- Insert initial user_books relationships
-INSERT INTO `user_books`(user_id, book_id, isRead, isWishlist, read_date) VALUES
+INSERT INTO `user_books`(user_id, book_id, is_read, is_wishlist, read_date) VALUES
 (3, 2, TRUE, FALSE, '2024-1-19'),
 (3, 1, FALSE, TRUE, NULL),
 (3, 3, FALSE, TRUE, NULL);
