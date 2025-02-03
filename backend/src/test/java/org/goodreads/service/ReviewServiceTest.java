4918d358fe85fcd5123098f2e8612056918a6f82
Andrei <andreimititelu97@gmail.com>
2025-02-04 00:38:08 +0200

    Added tests for reviewService

--- backend/src/test/java/org/goodreads/service/ReviewServiceTest.java
+++ backend/src/test/java/org/goodreads/service/ReviewServiceTest.java
@@ -1,23 +1,25 @@
 package org.goodreads.service;
 
-import org.aspectj.lang.annotation.Before;
 import org.greatreads.dto.ReviewDTO;
+import org.greatreads.exception.BookNotFoundException;
+import org.greatreads.exception.ReviewNotFoundException;
+import org.greatreads.exception.UserNotFoundException;
 import org.greatreads.model.Book;
 import org.greatreads.model.Review;
 import org.greatreads.model.User;
 import org.greatreads.repository.BookRepository;
 import org.greatreads.repository.ReviewRepository;
 import org.greatreads.repository.UserRepository;
-import org.greatreads.service.ReviewService;
 import org.greatreads.service.impl.ReviewServiceImpl;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Optional;
 
@@ -62,6 +64,31 @@ class ReviewServiceTest {
     }
 
     @Test
+    void testAddReview_BookNotFound() {
+        ReviewDTO reviewDTO = new ReviewDTO();
+        int bookId = 1;
+        Mockito.when(bookRepository.findById(bookId)).thenThrow(new BookNotFoundException(bookId));
+
+        assertThrows(BookNotFoundException.class, () -> reviewService.addReview(bookId, reviewDTO));
+    }
+
+    @Test
+    void testAddReview_UserNotFound() {
+        Book book = new Book();
+        book.setId(1);
+        int bookId = 1;
+
+        ReviewDTO reviewDTO = new ReviewDTO();
+        reviewDTO.setUserId(2);
+
+        Mockito.when(bookRepository.findById(bookId)).thenReturn(Optional.of(book));
+        Mockito.when(userRepository.findById(reviewDTO.getUserId()))
+                .thenThrow(new UserNotFoundException(reviewDTO.getUserId()));
+
+        assertThrows(UserNotFoundException.class, () -> reviewService.addReview(bookId, reviewDTO));
+    }
+
+    @Test
     void testUpdateReview() {
         User user = new User();
         user.setId(25);
@@ -89,6 +116,32 @@ class ReviewServiceTest {
     }
 
     @Test
+    void testUpdateReview_ReviewNotFound() {
+        ReviewDTO reviewDTO = new ReviewDTO();
+        int reviewId = 1;
+
+        Mockito.when(reviewRepository.findById(reviewId)).thenThrow(new ReviewNotFoundException(reviewId));
+        assertThrows(ReviewNotFoundException.class, () -> reviewService.updateReview(reviewId, reviewDTO));
+    }
+
+    @Test
+    void testUpdateReview_UserNotAllowed() {
+        ReviewDTO reviewDTO = new ReviewDTO();
+        reviewDTO.setUserId(2);
+
+        User user = new User();
+        user.setId(25);
+
+        Review review = new Review();
+        review.setId(1);
+        review.setUser(user);
+        int reviewId = 1;
+
+        Mockito.when(reviewRepository.findById(review.getId())).thenReturn(Optional.of(review));
+        assertThrows(IllegalArgumentException.class, () -> reviewService.updateReview(reviewId, reviewDTO));
+    }
+
+    @Test
     void testDeleteReview() {
         Review review = new Review();
         review.setId(1);
@@ -99,10 +152,11 @@ class ReviewServiceTest {
         Mockito.verify(reviewRepository, Mockito.times(1)).delete(Mockito.any(Review.class));
     }
 
-    //TODO testDeleteReview_ReviewNotFoundException
-    //TODO testAddReview_BookNotFoundException
-    //TODO testAddReview_UserNotFoundException
-    //TODO testUpdateReview_ReviewNotFoundException
-    //TODO testUpdateReview_UserNotAllowed
+    @Test
+    void testDeleteReview_ReviewNotFoundReview() {
+        int reviewId = 1;
+        Mockito.when(reviewRepository.findById(reviewId)).thenThrow(new ReviewNotFoundException(reviewId));
 
+        assertThrows(ReviewNotFoundException.class, () -> reviewService.deleteReview(reviewId));
+    }
 }
