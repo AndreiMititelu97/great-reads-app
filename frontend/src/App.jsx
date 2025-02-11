68c4bca4aae80da03a0eb516fb6f9305d8c19907
Andrei <andreimititelu97@gmail.com>
2025-02-11 23:47:42 +0200

    Create BooksComponent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import HeaderComponent from "./components/HeaderComponent.jsx";
+import BooksComponent from "./components/BooksComponent.jsx";
+import LoginComponent from "./components/LoginComponent.jsx";
+import {Route, Routes} from "react-router-dom";
+import FooterComponent from "./components/FooterComponent.jsx";
+import RegisterComponent from "./components/RegisterComponent.jsx";
 
 function App() {
 
   return (
     <>
         <HeaderComponent/>
+        <Routes>
+            <Route path="/" element={<LoginComponent/>}/>
+            <Route path="/books" element={<BooksComponent/>}/>
+            <Route path="/register" element={<RegisterComponent/>}/>
+        </Routes>
+        <FooterComponent/>
     </>
   )
 }
