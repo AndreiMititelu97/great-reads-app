edbd8b4bf20bb6d3e039e0889908f8fa4797baf7
Andrei Mititelu <andreimititelu@tutamail.com>
2025-02-22 16:57:18 +0200

    updated reader page

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -13,12 +13,12 @@ const HeaderComponent = () => {
                     sx={{ width: 50, height: 50, mr: 1 }}
                 />
                 <Typography variant="h6" sx={{ flexGrow: 1 }}>Great Reads</Typography>
-                {token && <Button color="inherit" component={Link} to="/reader">Reader Page</Button>}
+                {token && <Button color="inherit" component={Link} to="/reader">My Books</Button>}
                 <Button color="inherit" component={Link} to="/books">Books</Button>
                 |
                 <Button color="inherit" component={Link} to="/login">Login</Button>
                 <Button color="inherit" component={Link} to="/register">Register</Button>
-                <Button color="inherit" component={Link} to="/logout">Logout</Button>
+                {token && <Button color="inherit" component={Link} to="/logout">Logout</Button>}
             </Toolbar>
         </AppBar>
     );
--- frontend/src/components/reader/ReaderDashboardComponent.jsx
+++ frontend/src/components/reader/ReaderDashboardComponent.jsx
@@ -7,7 +7,7 @@ const ReaderDashboard = () => {
     return (
         <Container>
             <Typography variant="h4" gutterBottom>
-                Reader Dashboard
+                My Books
             </Typography>
             <Grid container spacing={3}>
                 <Grid item xs={12} md={6}>
